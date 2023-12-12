870168d5f769e696f83211b64cbe269b8620c934
millbark <149570355+millbark@users.noreply.github.com>
2023-12-13 01:39:35 +0900

    업로드2

--- portfolio-mill/js/main.js
+++ portfolio-mill/js/main.js
@@ -1,16 +1,5 @@
 
 $(function(){
-  $('.aboutMe p').eq(1).click(function(){
-    $('.about').animate({bottom:'-70px'},500);
-  });
-  //메인화면 글자 체인지
-  const hashtag = ["성실한","끈기있는","노력하는","호기심이 많은","발전하는","도움이 되는"];
-
-		const randomHashtag= setInterval(function() {
-				const num = Math.floor(Math.random() * hashtag.length);
-        $('.intro span').text(hashtag[num]);
-	},1000);
-  
   //포트폴리오 슬라이드
   
 $('.worksSlide').slick({
@@ -44,14 +33,6 @@ $('.works_navi').click(function(e){
   let current=$(this).attr('data-name');
   $('.worksView').removeClass('on');
   $('#' + current).addClass('on');
-});
-$('.works_navi').eq(0).click(function(){
-  $('.click').addClass('on');
-})
-$('.worksList li').click(function(e){
-  e.preventDefault();
-  $('.worksList li').removeClass('active');
-  let current=$(this).addClass('active')
 });
 
 //팝업
